b7047a5d995337273dfeded082047e0510dbdccb
Marc Weinberger <marc.weinberger@me.com>
2016-01-18 16:21:52 +0100

    inlined resizable css

--- examples/webpack/src/app.ts
+++ examples/webpack/src/app.ts
@@ -7,40 +7,40 @@ import {Resizable} from 'ng2-resizable/ng2-resizable';
     selector: 'app',
     directives: [Resizable],
     styles: [`
-    @import url("//fonts.googleapis.com/css?family=Lato:400,300,700");
+        @import url("//fonts.googleapis.com/css?family=Lato:400,300,700");
 
-    body {
-      color: #ffffff;
-      font-family: 'Lato', sans-serif;
-      text-align: center;
-    }
+        body {
+          color: #ffffff;
+          font-family: 'Lato', sans-serif;
+          text-align: center;
+        }
 
-    #sidebar {
-      display: block;
-      background: #51BBFE;
-      width: 260px;
-      max-width: 500px;
-      min-width: 165px;
-      min-height: 300px;
-      margin-right: 15px;
-      float: left;
-    }
+        #sidebar {
+          display: block;
+          background: #51BBFE;
+          width: 260px;
+          max-width: 500px;
+          min-width: 165px;
+          min-height: 300px;
+          margin-right: 15px;
+          float: left;
+        }
 
-    #main {
-      display: block;
-      background: #757761;
-      width: 500px;
-      height: 300px;
-      float: left;
-    }
+        #main {
+          display: block;
+          background: #757761;
+          width: 500px;
+          height: 300px;
+          float: left;
+        }
     `],
     template: `
-    <section id="sidebar" [resizable]="['right', 'bottom']">
-        <h2>Sidebar</h2>
-    </section>
-    <section id="main" [resizable]="['bottom']">
-        <h2>Main Content</h2>
-    </section>
+        <section id="sidebar" [resizable]="['right', 'bottom']">
+            <h2>Sidebar</h2>
+        </section>
+        <section id="main" [resizable]="['bottom']">
+            <h2>Main Content</h2>
+        </section>
     `
 })
 export class ExampleApp {
--- src/resizable.ts
+++ src/resizable.ts
@@ -8,7 +8,81 @@ import {NgClass, NgFor} from "angular2/common";
         '(document:mouseup)': 'onResizeEnd($event)',
     },
     directives: [NgClass, NgFor],
-    styleUrls: ['resizable.css'],
+    styles: [`
+        .resizable {
+          position: relative;
+        }
+
+        .resizable .grabber {
+          display: block;
+          width: 14px;
+          height: 14px;
+          line-height: 14px;
+          position: absolute;
+          z-index: 1;
+          -webkit-user-select: none;
+          -moz-user-select: none;
+          -ms-user-select: none;
+          user-select: none;
+          background: transparent;
+        }
+
+        .resizable .grabber:after {
+          content: '';
+          position: absolute;
+          display: block;
+          box-sizing: border-box;
+          border: 1px solid #ccc;
+        }
+
+        .resizable .grabber.right::after, .resizable .grabber.left::after {
+          border-width: 0 1px;
+          top: 50%;
+          margin: -10px 0 0 3.5px;
+          height: 20px;
+          width: 7px;
+        }
+
+        .resizable .grabber.top::after, .resizable .grabber.bottom::after {
+          border-width: 1px 0;
+          left: 50%;
+          margin: 3.5px 0 0 -10px;
+          width: 20px;
+          height: 7px;
+        }
+
+        .resizable .grabber.top {
+          cursor: row-resize;
+          width: 100%;
+          top: 0;
+          left: 0;
+          margin-top: -14px;
+        }
+
+        .resizable .grabber.right {
+          cursor: col-resize;
+          height: 100%;
+          right: 0;
+          top: 0;
+          margin-right: -14px;
+        }
+
+        .resizable .grabber.bottom {
+          cursor: row-resize;
+          width: 100%;
+          bottom: 0;
+          left: 0;
+          margin-bottom: -14px;
+        }
+
+        .resizable .grabber.left {
+          cursor: col-resize;
+          height: 100%;
+          left: 0;
+          top: 0;
+          margin-left: -14px;
+        }
+    `],
     template: `
         <ng-content></ng-content>
         <div class=grabber [ngClass]="dir" *ngFor="#dir of directions" (mousedown)="onResizeStart($event, dir)"></div>
